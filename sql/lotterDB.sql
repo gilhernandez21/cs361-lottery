b7fde88f2fbfeeec7425ade7bd6e4534b3023867
gilhernandez21 <49378316+gilhernandez21@users.noreply.github.com>
2020-03-02 00:14:37 -0600

    Update lotterDB.sql
    added a batch ID so we can determine which lottery numbers belong to what batch

--- sql/lotterDB.sql
+++ sql/lotterDB.sql
@@ -8,6 +8,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `lotteryDB` (
     'lotteryID' int(11) NOT NULL auto_increment,
+    'batch_ID' int(11) Not NULL,
   `number_01` int(11) NOT NULL,
   `number_02` int(11) NOT NULL,
   `number_03` int(11) NOT NULL,
@@ -26,7 +27,7 @@ CREATE TABLE `users` (
 
 --input some test data for debugging puporses
 
-Insert into 'lotteryDB'('number_01','number_02','number_03','number_04','number_05','date_Generated') values (01,02,03,04,05,'3/1/2020'),(06,07,08,09,10,'2/29/2020')
+Insert into 'lotteryDB'('batch_ID','number_01','number_02','number_03','number_04','number_05','date_Generated') values (01,01,02,03,04,05,'3/1/2020'),(01,06,07,08,09,10,'2/29/2020')
 ;
 
 Insert into 'users'('user_Name','user_Password','date_login','date_Create') values ('UserName01','Password01','2/2/20','3/1/19'),('UserName02','Password02','1/1/20','2/28/19')
